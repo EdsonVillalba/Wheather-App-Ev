42c48fd5797c29eb73449d2303060921172bc97f
Edson Villalba <edson.villalba92@gmail.com>
2023-08-03 15:48:40 -0400

    LINK layout modificated

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-const LINK = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0";
+const LINK = "http://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0";
 
 export const metadata = {
   title: "Weather App Challenge",
